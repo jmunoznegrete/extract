849a0e5a35a6a39d688318e1430e5286b3f0b887
Ubuntu <jmunoznegrete@gmail.com>
2018-03-11 16:03:14 +0100

    Add .gitignore file + fixed __main__ when importing calc.py

--- calc_y.py
+++ calc_y.py
@@ -59,67 +59,68 @@ class day_Serie(object):
         for i in range(len(self.Open)):
             print "i=",self.Open[i]
 ## --------------------------------------------------------------
-## 
-vector_y = y_result('rawData/yVector')
-
-with open('rawData/EURUSDM1.csv', 'r+') as fin:
-    status_eur = status()
-    i = 0
-    for line in fin:
-        if i==0:
-            i = i + 1
-            continue
-        valores = line.split(',')    
-        if status_eur.CAPTURING_FIRST_BAR:
-            ## capturing the first bar of the day and skipping if not
-            ## something like 23:xx
-            if not (valores[0][11:13] == '23'):
+    ## 
+if __name__ == "__main__":
+    vector_y = y_result('rawData/yVector')
+    
+    with open('rawData/EURUSDM1.csv', 'r+') as fin:
+        status_eur = status()
+        i = 0
+        for line in fin:
+            if i==0:
+                i = i + 1
                 continue
-            tmp_date = strptime(valores[0],"%d.%m.%Y %H:%M:%S")
-            current_date = datetime(tmp_date[0], tmp_date[1], tmp_date[2],
-                                    tmp_date[3], tmp_date[4], tmp_date[5])
-
-            ## if Friday or Saturday skip these values
-            if current_date.weekday() == 5 or \
-                current_date.weekday() ==6:
-                continue
-            
-            ## date of first bar is already stored in current_date
-            ## and values OHLC stored as first values
-            status_eur.CAPTURING_FIRST_BAR = False
-            tmpDaySerie = day_Serie(valores[1], valores[2], 
-                                    valores[3], valores[4])
-
-        else:
-            ## date of first bar is already stored in current_date
-            ## now we are going to extract the follwing bar values onto a list
-            ## and decide de value for y
-            tmp_date = strptime(valores[0],"%d.%m.%Y %H:%M:%S")
-            current_time = datetime(tmp_date[0], tmp_date[1], tmp_date[2],
-                                    tmp_date[3], tmp_date[4], tmp_date[5])
-
-            if (current_time < 
-                    current_date + timedelta(minutes=cfg.EXPIRE_ORDER_TIME)):
-                tmpDaySerie.addValues(valores[1], valores[2],
-                                        valores[3], valores[4])    
-
+            valores = line.split(',')    
+            if status_eur.CAPTURING_FIRST_BAR:
+                ## capturing the first bar of the day and skipping if not
+                ## something like 23:xx
+                if not (valores[0][11:13] == '23'):
+                    continue
+                tmp_date = strptime(valores[0],"%d.%m.%Y %H:%M:%S")
+                current_date = datetime(tmp_date[0], tmp_date[1], tmp_date[2],
+                                        tmp_date[3], tmp_date[4], tmp_date[5])
+    
+                ## if Friday or Saturday skip these values
+                if current_date.weekday() == 5 or \
+                    current_date.weekday() ==6:
+                    continue
+                
+                ## date of first bar is already stored in current_date
+                ## and values OHLC stored as first values
+                status_eur.CAPTURING_FIRST_BAR = False
+                tmpDaySerie = day_Serie(valores[1], valores[2], 
+                                        valores[3], valores[4])
+    
             else:
-                y = evaluate_y(tmpDaySerie, 
-                                cfg.EXPECTED_MOVEMENT,
-                                cfg.SPREAD)
-                print "Resultado = ", y
-                vector_y.app(valores[0], y)
-                status_eur.CAPTURING_FIRST_BAR = True
-                break
-
-##tmpDaySerie.toFile()
-##vector_y.toFile()
-            
-
-## Iteracion hasta final de fichero:
-##  * leo fecha
-##  * busco hora de compra 23:00 + D [+ 5 min]: hay veces que a las 23:00 vol=0
-##  * completo la lista dede Hora de compra hasta F.
-##  * comparo y escribo las etiquetas en el fichero Y
-##  * chequeo errores: si hay alguno que no se compra o algun dia que falta
-##    para revisarlo.
+                ## date of first bar is already stored in current_date
+                ## now we are going to store the follwing bar values in a list
+                ## and decide de value for y
+                tmp_date = strptime(valores[0],"%d.%m.%Y %H:%M:%S")
+                current_time = datetime(tmp_date[0], tmp_date[1], tmp_date[2],
+                                        tmp_date[3], tmp_date[4], tmp_date[5])
+    
+                if (current_time < 
+                        current_date + timedelta(minutes=cfg.EXPIRE_ORDER_TIME)):
+                    tmpDaySerie.addValues(valores[1], valores[2],
+                                            valores[3], valores[4])    
+    
+                else:
+                    y = evaluate_y(tmpDaySerie, 
+                                    cfg.EXPECTED_MOVEMENT,
+                                    cfg.SPREAD)
+                    print "Resultado = ", y
+                    vector_y.app(valores[0], y)
+                    status_eur.CAPTURING_FIRST_BAR = True
+                    break
+    
+    ##tmpDaySerie.toFile()
+    ##vector_y.toFile()
+                
+    
+    ## Iteracion hasta final de fichero:
+    ##  * leo fecha
+    ##  * busco hora de compra 23:00 + D [+ 5 min]: hay veces que a las 23:00 vol=0
+    ##  * completo la lista dede Hora de compra hasta F.
+    ##  * comparo y escribo las etiquetas en el fichero Y
+    ##  * chequeo errores: si hay alguno que no se compra o algun dia que falta
+    ##    para revisarlo.

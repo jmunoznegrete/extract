1daf3a0aaff39b1cb3b52a198bae16744ae70e3b
Ubuntu <jmunoznegrete@gmail.com>
2018-03-12 22:10:46 +0100

    Add filterdate.py to filter zero volume dates from the input file

---
+++ filterdate.py
@@ -0,0 +1,31 @@
+import sys
+
+def getFileOut(FileIn):
+    fileOut = FileIn.split(".csv")[0] + "_fi.csv"
+    return fileOut
+
+try:
+    FileIn = sys.argv[1]
+    FileOut = getFileOut(sys.argv[1])
+except IndexError :
+    print "Error: usage: python filterdate.py filename.csv"
+    exit(0)
+
+with open(FileIn, 'r') as fin:
+    with open(FileOut, 'a+') as fout:
+        i = 0
+        for line in fin:
+            if i== 0:
+                fout.write(line)
+                i = i+1
+                continue
+
+            line = line.split('\r')[0]
+            if (float(line.split(',')[5]) == 0.0):
+                continue
+            indice = line.find('.000 GMT')
+            lineout = line[0:indice]+line[indice+13:]+'\n'
+            print lineout[:-1]
+            fout.write(lineout)
+
+print 'Done with ', FileIn,' and ', FileOut
